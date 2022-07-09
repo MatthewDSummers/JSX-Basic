9180479bf471b30b75c0cba9b34f926d22cc21f5
Matthew Summers <imadethisforcoding@gmail.com>
2022-07-09 15:21:28 +0800

    basic JSX practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, Dojo!</h1>
+      <p>Some things <strong>I need to do</strong></p>
+      <ul>
+        <li>Practice MongoDB</li>
+        <li>Practice Express</li>
+        <li>Practice React</li>
+        <li>Practice Node.js</li>
+      </ul>
     </div>
   );
 }
